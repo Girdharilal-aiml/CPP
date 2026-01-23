76ff880bc9384951facb8ab361601c8b660acc13
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-01-23 21:00:29 +0500

    Update Q2.cpp

--- PRACTICE/Q2.cpp
+++ PRACTICE/Q2.cpp
@@ -10,18 +10,18 @@ int main(){
     cout<<"Enter Username: ";
     cin>>username;
     cout<<"Enter Password: ";
-    cin>>pass;
+    cin>>password;
 
-    if(username== "USER" && pass== 0000){
+    if(username== "USER" && password== 0000){
         cout<<"Access Granted"<<endl;
     }else{
         cout<<"Access Denied"<<endl;
-        if(username !="USER" && pass != 0000){
+        if(username !="USER" && password != 0000){
             cout<<"Wrong user and pass"<<endl;
         }
         else if(username!="USER"){
             cout<<"Wrong user"<<endl;
-        }else if(pass != 0000){
+        }else if(password != 0000){
             cout<<"Wrong pass"<<endl;
         }
     }
