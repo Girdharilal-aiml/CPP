e3f73cf53f37b0702a00bfeec7e72b91589c1433
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-01-25 19:49:04 +0500

    Create Q3.cpp

---
+++ PRACTICE/Q3.cpp
@@ -0,0 +1,13 @@
+/*
+2. Create a C++ program to handle student marks management. The program should allow users to
+input marks for students in four courses: PF, OOP, DLD & ICT. It should then calculate the total
+marks, average marks, and determine the grade for each student based on their average.
+Use the following grading metrics:
+90 or above: Grade A
+80-89: Grade B
+70-79: Grade C
+60-69: Grade D
+Below 60: Grade F
+*/
+
+
