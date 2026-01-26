637565368a05c34452ff6c7d85384d49b94c7fd2
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-01-26 20:44:33 +0500

    Update Q7.cpp

--- PRACTICE/Q7.cpp
+++ PRACTICE/Q7.cpp
@@ -3,4 +3,23 @@ Imagine you are working on a project where the efficient generation of odd numbe
 Your task is to write a C++ function that prints an odd number after every sequence of 5 even
 numbers in the range provided by the user. The catch is that you need to optimize the program for
 both correctness and efficiency.
+
+for example :
+Start range: 0
+ending range: 20
+ouput:
+0
+2
+4
+6
+8
+9
+10
+12
+14
+16
+18
+19
+20
 */
+
