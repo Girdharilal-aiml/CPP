9a08b3482f7023c384e4eb407b7c523634be80a7
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-01-26 20:39:24 +0500

    Update Q4.cpp

--- PRACTICE/Q4.cpp
+++ PRACTICE/Q4.cpp
@@ -1,5 +1,5 @@
 /*
-3. Write a program to make a simple calculator performing the four basic operations (+, -, *, /) on
+Write a program to make a simple calculator performing the four basic operations (+, -, *, /) on
 two numbers input by user. The following conditions must be satisfied:
 a) A ‘+’ sign must not be used for addition.
 b) You can only use a maximum of 3 variables. No more variables are allowed.
