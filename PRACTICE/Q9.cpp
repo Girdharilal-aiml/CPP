d960111bc686a1b8e5ed405fb400d061cb20b388
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-01-26 22:14:20 +0500

    Create Q9.cpp

---
+++ PRACTICE/Q9.cpp
@@ -0,0 +1,10 @@
+/*
+Imagine you are tasked with creating a program to manage a library's book inventory. Each
+book has attributes such as title, author, publication year, and genre. Design a struct that
+effectively represents a book as a real-world entity. Then, write a C++ program that utilizes
+this struct to demonstrate the creation, modification, and display of book information.
+a) Extend the program to manage multiple books in an array.
+b) Implement a function to search for a book by title or author.
+c) Allow the user to input new books and update existing book information
+interactively.
+*/
