b7693ba0e941af43865deb5a725f5d742fa0c630
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-01-26 20:40:37 +0500

    Create Q7.cpp

---
+++ PRACTICE/Q7.cpp
@@ -0,0 +1,6 @@
+/*
+Imagine you are working on a project where the efficient generation of odd numbers is crucial.
+Your task is to write a C++ function that prints an odd number after every sequence of 5 even
+numbers in the range provided by the user. The catch is that you need to optimize the program for
+both correctness and efficiency.
+*/
