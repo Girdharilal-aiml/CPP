36bf7506cdc80f52f195079e2b5f95490dac8a98
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-01-27 20:42:38 +0500

    Create Q14.cpp

---
+++ PRACTICE/Q14.cpp
@@ -0,0 +1,7 @@
+/*
+Time to code a program that manages an ever-growing hungry integer array! The array
+starts small with a size of 5, but every time you keep stuffing it with numbers and it gets
+full, it will double in size to satisfy its appetite. Once you're done feeding it all the elements,
+the array will go on a diet and shrink down to perfectly fit the number of elements it holds.
+No wasted space, no extra fluff—just a happy, well-fed array.
+*/
