fa98f909450537da8e3e626e999b190765bcd54a
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-01-27 20:42:42 +0500

    Create Q13.cpp

---
+++ PRACTICE/Q13.cpp
@@ -0,0 +1,7 @@
+/*
+You need to make a program that works like a Matrix Wizard. First, it will ask the user for
+the size of a 2D matrix and then create it using memory magic (dynamic allocation). Next,
+the program will fill the matrix with numbers given by the user. After that, it will show off
+by doing matrix addition, subtraction, and multiplication (only if the sizes match). Finally,
+it will display the results and clean up the memory like a good, tidy wizard.
+*/
