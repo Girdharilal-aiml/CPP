522212fbbb3f76f7b3722e38379944d8a4f4b49c
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-01-27 20:42:47 +0500

    Create Q12.cpp

---
+++ PRACTICE/Q12.cpp
@@ -0,0 +1,7 @@
+/*
+Create a program that moonlights as a basic calculator, wielding the power of pointer
+arithmetic to crunch numbers like a pro. It should charm the user into entering two integers,
+perform addition, subtraction, multiplication, or division (depending on the user's mood),
+and then proudly display the result. Use pointers to juggle values between functions and
+make the magic happen.
+*/
