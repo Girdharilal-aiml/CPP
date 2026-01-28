df63948f5dc7096ef45c6f46e0348d1d0b292184
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-01-28 19:07:04 +0500

    Update Q11.cpp

--- PRACTICE/Q11.cpp
+++ PRACTICE/Q11.cpp
@@ -3,3 +3,10 @@ Implement a function that dynamically allocates an array of integers. The functi
 take an integer parameter specifying the number of elements to allocate and return a pointer
 to the allocated array.
 */
+
+#include"iostream"
+using namespace std;
+
+int main(){
+    
+}
