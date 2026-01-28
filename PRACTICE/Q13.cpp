0bc8bdfa85df49d5a65934bb3d00bf99a1ecb7b8
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-01-28 20:01:35 +0500

    Update Q13.cpp

--- PRACTICE/Q13.cpp
+++ PRACTICE/Q13.cpp
@@ -5,3 +5,14 @@ the program will fill the matrix with numbers given by the user. After that, it 
 by doing matrix addition, subtraction, and multiplication (only if the sizes match). Finally,
 it will display the results and clean up the memory like a good, tidy wizard.
 */
+
+#include"iostream"
+using namespace std;
+
+int main(){
+    int c,r;
+    cout<<"Enter Column size: ";
+    cin>>c;
+    
+
+}
