75fe712134074731eb5e7b41b149f2133882362c
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-02-07 12:39:24 +0500

    Update Q15.cpp

---
+++ PRACTICE/Q15.cpp
@@ -0,0 +1,14 @@
+/*
+1. Design a Book class for a library system with attributes:
+   - title
+   - author
+   - price
+
+   Requirements:
+   - Default constructor → "Untitled", "Anonymous", price = 0.0
+   - Parameterized constructor → set all values
+   - Copy constructor → duplicate a book
+   - Constructor with default arguments → only title required,
+     author = "Unknown", price = 0.0
+   - Create objects using all constructors and display details
+*/
