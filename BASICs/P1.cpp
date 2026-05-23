4e0c115b90774598608fac439c4317b12a6c6330
Girdhari lal <girdharilal.aiml@gmail.com>
2026-05-23 19:50:29 +0500

    Update P1.cpp

--- BASICs/P1.cpp
+++ BASICs/P1.cpp
@@ -56,5 +56,6 @@ public:
         cout << "----------------------------------------" << endl;
         cout << "STUDENT RECORD" << endl;
         cout << "Name    : " << getName() << endl;
+        cout << "ID      : " << getID() << endl;
     }
 };
