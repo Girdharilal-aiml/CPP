2565599100dce11375f467d3cde63306630ba1fa
Girdhari lal <girdharilal.aiml@gmail.com>
2026-05-27 13:28:32 +0500

    Update Q15.cpp

--- PRACTICE/Q15.cpp
+++ PRACTICE/Q15.cpp
@@ -3,3 +3,6 @@
 // Rule: Write a function increaseTemp(double degrees). 
 // It must only increase the temperature if degrees is positive AND the resulting temperature does not exceed maxLimit. 
 // If it succeeds, return true; otherwise, do not change the temperature and return false.
+
+#include "iostream"
+
