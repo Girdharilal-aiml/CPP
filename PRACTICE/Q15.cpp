dc863377189ee4dbc2614220cd179ee1cd98f246
Girdhari lal <girdharilal.aiml@gmail.com>
2026-05-27 13:20:32 +0500

    Update Q15.cpp

---
+++ PRACTICE/Q15.cpp
@@ -0,0 +1,5 @@
+// 1. The Smart Thermostat
+// Design a RoomHeater entity containing targetTemperature and maxLimit.
+// Rule: Write a function increaseTemp(double degrees). 
+// It must only increase the temperature if degrees is positive AND the resulting temperature does not exceed maxLimit. 
+// If it succeeds, return true; otherwise, do not change the temperature and return false.
