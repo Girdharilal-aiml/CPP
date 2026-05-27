1fc338ebbc209eccb3e4eca7d81c9ce8ea018375
Girdhari lal <girdharilal.aiml@gmail.com>
2026-05-27 13:20:45 +0500

    Update Q19.cpp

--- PRACTICE/Q19.cpp
+++ PRACTICE/Q19.cpp
@@ -1,1 +1,4 @@
 
+// 5. The Real Estate App
+// Rule: Write a generic utility tool named findBetterProperty. It must accept any two objects, provided they are of the exact same data type.
+// Rule: Inside this utility, call a method .getSquareFootage() on both objects and return the object that has the larger value.
