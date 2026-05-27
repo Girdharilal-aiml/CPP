4812fb00203ddc600a379126ff12f2af001f10d3
Girdhari lal <girdharilal.aiml@gmail.com>
2026-05-27 13:21:38 +0500

    Update Q22.cpp

---
+++ PRACTICE/Q22.cpp
@@ -0,0 +1,3 @@
+// 8. The Space Fleet Shield Check: A Mothership entity manages smaller fighters.
+// Rule: Write a method checkFleetShields(fighters, size) that accepts an array of Fighter objects.
+// Rule: If the size is 0 or less, return false. Loop through the array. If any ship in the array has a .getShieldLevel() less than 20.0, the fleet is vulnerable (return false). Otherwise, return true.
