71d548888bfb3e99dcff7772c46530a39e2d449b
Girdhari lal <girdharilal.aiml@gmail.com>
2026-05-27 13:20:39 +0500

    Update Q17.cpp

---
+++ PRACTICE/Q17.cpp
@@ -0,0 +1,5 @@
+// 3. The Coffee Machine
+// Design an EspressoMachine entity with waterLevel and coffeeBeansLevel.
+// Rule: Write a brew(int shots) function. One shot requires 30ml of water and 15g of beans. 
+// Before brewing, validate that there is strictly enough of both resources simultaneously. 
+// If so, deduct the resources and return true; otherwise, return false.
