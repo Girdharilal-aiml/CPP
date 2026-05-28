423dc5ca3801fdfe4d141aec2d58539d0b0a57fd
Girdhari lal <girdharilal.aiml@gmail.com>
2026-05-28 12:30:53 +0500

    Update Q16.cpp

--- PRACTICE/Q16.cpp
+++ PRACTICE/Q16.cpp
@@ -2,3 +2,7 @@
 // Design a system for a bank. You have a general concept called Transaction, which contains a transactionID.
 // Rule: A "Transaction" is just a conceptual idea; the system must strictly prevent another programmer from creating a direct object of a generic transaction. It can only be a Deposit or a Withdrawal.
 // Rule: Every transaction must be able to calculateFee(). However, the mathematical formula for this fee depends entirely on whether it is a deposit or a withdrawal. Force this rule in your architecture.
+
+#include"iostream"
+
+
