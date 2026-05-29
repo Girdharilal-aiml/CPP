8eeeed245ce773f486878827ef88d34aeabcc908
Girdhari lal <girdharilal.aiml@gmail.com>
2026-05-29 12:17:03 +0500

    Update Q17.cpp

--- PRACTICE/Q17.cpp
+++ PRACTICE/Q17.cpp
@@ -3,3 +3,6 @@
 // Rule: Write a brew(int shots) function. One shot requires 30ml of water and 15g of beans. 
 // Before brewing, validate that there is strictly enough of both resources simultaneously. 
 // If so, deduct the resources and return true; otherwise, return false.
+
+#include "iostream"
+
