b163c16be82412acc122cb841988c20a61fe5743
Girdhari lal <girdharilal.aiml@gmail.com>
2026-05-29 12:17:49 +0500

    Update Q17.cpp

--- PRACTICE/Q17.cpp
+++ PRACTICE/Q17.cpp
@@ -15,5 +15,6 @@ private:
 public:
     EspressoMachine(double water, double beans) : waterLevel(water), coffeeBeansLevel(beans) {}
     bool brew(int shots){
+        double requiredWater = shots * 30;
     }  
 };
