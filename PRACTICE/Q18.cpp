570cdb26a0d53f168ca6f6542efcc580edc1b6e2
Girdhari lal <girdharilal.aiml@gmail.com>
2026-05-30 13:20:53 +0500

    Update Q18.cpp

--- PRACTICE/Q18.cpp
+++ PRACTICE/Q18.cpp
@@ -2,3 +2,8 @@
 // Create a general User entity containing an email.
 // Rule: The system must strictly prevent creating a plain User.
 // Rule: Create two specific entities: Client and Freelancer. Both must implement a getPlatformRating() method, but the logic for calculating the rating is unique to each entity.
+
+#include "iostream"
+
+
+
