0cd01cac303314a323c04ea9cf0aed22e890aa50
Girdhari lal <girdharilal.aiml@gmail.com>
2026-05-31 13:28:22 +0500

    Update Q20.cpp

--- PRACTICE/Q20.cpp
+++ PRACTICE/Q20.cpp
@@ -1,3 +1,7 @@
 // 6. HR Performance Tracker
 // Rule: Write a generic utility named getTopPerformer that takes two objects of the exact same type. 
 // Inside the function, compare them using their .getKPI() method and return the object with the higher KPI.
+
+#include "iostream"
+
+
