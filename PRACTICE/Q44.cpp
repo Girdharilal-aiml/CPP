9cff80eb9044be72164200b0b76483e927ed56d4
Girdhari lal <girdharilal.aiml@gmail.com>
2026-06-02 12:23:41 +0500

    Update Q44.cpp

---
+++ PRACTICE/Q44.cpp
@@ -0,0 +1,3 @@
+// 30. The Fleet Backup System (The Final Boss):
+// Rule: Write a system that takes an array of exactly 10 GasVehicle objects and writes the entire array to a file as a single raw block of memory bytes in one single operation.
+// Rule: If the file fails to open during this process, halt execution by signaling an error using a simple text message string. Capture this error safely in main().
