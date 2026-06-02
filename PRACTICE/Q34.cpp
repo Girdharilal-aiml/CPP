7035754a6ee1d149802ee196f5e9cab944e15bcd
Girdhari lal <girdharilal.aiml@gmail.com>
2026-06-02 12:24:11 +0500

    Update Q34.cpp

---
+++ PRACTICE/Q34.cpp
@@ -0,0 +1,4 @@
+// 20. The Secure Cloud Balancer:
+// Rule: Create a general concept Node. Create a DatabaseNode entity that implements it.
+// Rule: Create a generic storage utility (like in Question 11) capable of holding polymorphic pointers to the general Node concept.
+// Rule: In main(), instantiate this storage, create two dynamic DatabaseNode entities, and add them to the storage.
