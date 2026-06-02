89d6587d25aceccbcf6d414e9b69495c6e2f35a4
Girdhari lal <girdharilal.aiml@gmail.com>
2026-06-02 12:23:56 +0500

    Update Q39.cpp

---
+++ PRACTICE/Q39.cpp
@@ -0,0 +1,2 @@
+// 25. Network Timeout Fallbacks: A connectServer() function might halt and signal an integer (like error code 404) OR it might signal a text string (like "Timeout").
+// Rule: Write the execution logic in main() to safely attempt the connection, providing one safety net for the integer, a separate safety net for the string, and a final universal safety net to catch literally anything else that might halt the flow.
