b8ec9abeb4fc035b7dcc54725426a08d743c9f9b
Girdhari lal <girdharilal.aiml@gmail.com>
2026-06-02 12:24:15 +0500

    Update Q33.cpp

---
+++ PRACTICE/Q33.cpp
@@ -0,0 +1,2 @@
+// 19. The Gadget Comparer: Create a base Gadget concept that must provide getBatteryLife(). Create Phone and Tablet entities that implement this.
+// Rule: In main(), use a generic utility function (like the one from Question 5) to compare two Phone objects and return the one with the better battery life.
