efa7a057e9a1b344ab4b70dc197bc5154992d9a4
Girdhari lal <girdharilal.aiml@gmail.com>
2026-06-02 12:24:34 +0500

    Update Q28.cpp

---
+++ PRACTICE/Q28.cpp
@@ -0,0 +1,4 @@
+// 14. The University Persona: At a university, you have a Person entity with an idNumber. A Student is a Person. An Employee is a Person.
+// Rule: A TeachingAssistant acts as both a Student and an Employee simultaneously, combining the traits of both.
+// Crucial Rule: Even though a TA combines two roles, they are still a single physical human being. Your architecture must ensure that a TeachingAssistant only has one idNumber in memory to prevent ambiguous data.
+// Rule: Ensure the TA's constructor bypasses the intermediate roles to directly initialize the idNumber of the core Person.
