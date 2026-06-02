efa9e5665fc5f0da41281f126f18a8fc06ee9c43
Girdhari lal <girdharilal.aiml@gmail.com>
2026-06-02 12:23:49 +0500

    Update Q41.cpp

---
+++ PRACTICE/Q41.cpp
@@ -0,0 +1,3 @@
+// 27. User Settings Modifier:
+// Rule: Open config.txt. Read the first word into a string variable and the next number into an integer.
+// Rule: Modify the integer (e.g., add 1 to it), clear the file's previous contents entirely, and write the new settings back to the file.
