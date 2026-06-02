581c7ad6993aaaf7ed142e020977ca8d250a1745
Girdhari lal <girdharilal.aiml@gmail.com>
2026-06-02 12:24:38 +0500

    Update Q27.cpp

---
+++ PRACTICE/Q27.cpp
@@ -0,0 +1,3 @@
+// 13. The Print Spooler:
+// Rule: Design a generic Spooler entity. It holds an array of 5 generic jobs.
+// Rule: Provide an addJob(document) function that increments a counter when a job is added, alongside a getJobCount() function to return the current number of jobs.
