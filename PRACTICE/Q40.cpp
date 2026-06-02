3708485d9de04f82797d396ee226ca940794057f
Girdhari lal <girdharilal.aiml@gmail.com>
2026-06-02 12:23:52 +0500

    Update Q40.cpp

---
+++ PRACTICE/Q40.cpp
@@ -0,0 +1,3 @@
+// 26. The Persistent Logger:
+// Rule: Open a text file called server_logs.txt in a way that allows you to add new log messages to the very end of the file without erasing the old ones.
+// Rule: After adding a new log, move the internal file reading cursor back to the absolute beginning of the file, read all the contents, and print them to the console.
