afc7ade50d78f4a385997fee7d02d0d2ba572db8
Girdhari lal <girdharilal.aiml@gmail.com>
2026-06-02 12:23:44 +0500

    Update Q43.cpp

---
+++ PRACTICE/Q43.cpp
@@ -0,0 +1,3 @@
+// 29. Game Save State (Raw Memory Retrieval):
+// Rule: Open savegame.dat. Read a raw block of memory bytes from the disk directly back into an empty PlayerStats object (containing int level, double health, int gold).
+// Rule: Extract the entire object structure in a single disk operation, not variable-by-variable.
