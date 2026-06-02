ec4c11b2c34046a74e7786501cac8bf2935ebcef
Girdhari lal <girdharilal.aiml@gmail.com>
2026-06-02 12:24:08 +0500

    Update Q35.cpp

---
+++ PRACTICE/Q35.cpp
@@ -0,0 +1,3 @@
+// 21. The Login Firewall: Design a SecuritySystem entity with a method authenticate(string password).
+// Rule: If the password length is less than 8 characters, the system must immediately halt its normal execution flow and signal an error by ejecting the exact text message: "Password too short".
+// Rule: In your main() function, execute this method and safely capture this exact text message, printing it to the screen without crashing the program.
