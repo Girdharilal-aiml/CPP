ba6bf781e630946b2b96b999cc313812dec7a95c
Girdhari lal <girdharilal.aiml@gmail.com>
2026-06-03 12:29:59 +0500

    Update Q23.cpp

--- PRACTICE/Q23.cpp
+++ PRACTICE/Q23.cpp
@@ -1,3 +1,6 @@
 // 9. Cyber Security Network:
 // Rule: Design a method isNetworkStable(nodes, count) that accepts an array of Server objects.
 // Rule: Traverse the array. If any server returns a value greater than 500 from .getPing(), the network is unstable (return false). If all are fine, return true.
+
+#include "iostream"
+
