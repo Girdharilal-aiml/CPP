a6b1b8336b3d3f5f5dfdc94ccbb36e263a9acd9b
Girdhari lal <girdharilal.aiml@gmail.com>
2026-06-04 19:37:32 +0500

    Update Q24.cpp

--- PRACTICE/Q24.cpp
+++ PRACTICE/Q24.cpp
@@ -1,3 +1,6 @@
 // 10. Drone Delivery Swarm: A DeliveryHub entity dispatches drones.
 // Rule: Write a method clearForTakeoff(swarm, numDrones) that accepts an array of Drone objects. 
 // Iterate through the swarm. If any drone has a .getBatteryPercentage() below 15.0, abort the takeoff (return false).
+
+#include"iostream"
+
