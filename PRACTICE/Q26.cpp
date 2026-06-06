d334bf53e8c09e12600184df898260e9cb8f9afa
Girdhari lal <girdharilal.aiml@gmail.com>
2026-06-06 13:52:21 +0500

    Update Q26.cpp

--- PRACTICE/Q26.cpp
+++ PRACTICE/Q26.cpp
@@ -1,3 +1,7 @@
 // 12. The Task Queue:
 // Rule: Write a storage entity ProcessQueue. It must contain an array of size 5 to hold tasks of any one specific data type.
 // Rule: Write a function enqueue(task) that adds to the array, but silently ignores the addition if the queue has already reached its limit of 5.
+
+#include "iostream"
+
+
