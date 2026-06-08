8fb6592c9a6afb50a529b2e45be1acf4a6059a6f
Girdhari lal <girdharilal.aiml@gmail.com>
2026-06-08 22:19:26 +0500

    Update Q30.cpp

--- PRACTICE/Q30.cpp
+++ PRACTICE/Q30.cpp
@@ -1,2 +1,5 @@
 // 16. Smart Home Gadgets: A base Device has a serialNumber set via its constructor. A Camera and a Microphone are both Devices.
 // Rule: A SmartDisplay combines both a Camera and a Microphone. Write the constructor for SmartDisplay to demonstrate how it properly initializes the serialNumber in the core Device base class without causing duplicate data.
+
+#include "iostream"
+
