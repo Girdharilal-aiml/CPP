7376322f6d8f31cbf09b78c5349d45640ea1d119
Girdhari lal <girdharilal.aiml@gmail.com>
2026-06-09 13:50:32 +0500

    Update Q31.cpp

--- PRACTICE/Q31.cpp
+++ PRACTICE/Q31.cpp
@@ -1,3 +1,6 @@
 // 17. The Media Player: A MediaFile has a filePath. AudioTrack and VideoTrack are MediaFiles.
 // Rule: A Movie combines both an AudioTrack and a VideoTrack. Design the classes so Movie has only one filePath.
 // Rule: Implement a play() method in Movie that specifically calls the individual play() methods of both the AudioTrack and VideoTrack.
+
+#include "iostream"
+
