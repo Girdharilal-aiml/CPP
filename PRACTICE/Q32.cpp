e5bcc2c402f88525cde32e51a3cb2af1b7b08415
Girdhari lal <girdharilal.aiml@gmail.com>
2026-06-10 19:52:14 +0500

    Update Q32.cpp

--- PRACTICE/Q32.cpp
+++ PRACTICE/Q32.cpp
@@ -1,2 +1,6 @@
 // 18. The Zoo Enclosure: You have a base concept Animal that must be able to makeSound(). Lion and Monkey are specific animals.
 // Rule: Write a main() function that creates an array capable of holding 3 pointers to the general Animal concept. Assign a Lion and a Monkey dynamically (using new) to this array, and trigger their sounds via a loop.
+
+#include "iostream"
+
+
