c4a0eaec7bb6360abc61c3c2b0d9d92ad5aafbb9
Girdhari lal <girdharilal.aiml@gmail.com>
2026-06-10 19:52:19 +0500

    Update Q32.cpp

--- PRACTICE/Q32.cpp
+++ PRACTICE/Q32.cpp
@@ -2,5 +2,6 @@
 // Rule: Write a main() function that creates an array capable of holding 3 pointers to the general Animal concept. Assign a Lion and a Monkey dynamically (using new) to this array, and trigger their sounds via a loop.
 
 #include "iostream"
+using namespace std;
 
 
