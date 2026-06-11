38a7d4c613015f1a899f78b93c758381c421c8f2
Girdhari lal <girdharilal.aiml@gmail.com>
2026-06-11 13:15:30 +0500

    Update Q34.cpp

--- PRACTICE/Q34.cpp
+++ PRACTICE/Q34.cpp
@@ -2,3 +2,5 @@
 // Rule: Create a general concept Node. Create a DatabaseNode entity that implements it.
 // Rule: Create a generic storage utility (like in Question 11) capable of holding polymorphic pointers to the general Node concept.
 // Rule: In main(), instantiate this storage, create two dynamic DatabaseNode entities, and add them to the storage.
+
+#include "iostream"
