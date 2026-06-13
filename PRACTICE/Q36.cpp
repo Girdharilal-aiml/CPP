41a9a5fb20b2c455f69d3cc7ad25ee5c3a1d0e36
Girdhari lal <girdharilal.aiml@gmail.com>
2026-06-13 23:48:00 +0500

    Update Q36.cpp

--- PRACTICE/Q36.cpp
+++ PRACTICE/Q36.cpp
@@ -1,3 +1,7 @@
 // 22. Sensor Calibration: Design a Sensor entity with a readData() method.
 // Rule: If the sensor reads a negative value, halt flow and signal a standard C++ system runtime error object containing the message "Negative reading detected".
 // Rule: Safely capture this specific type of system error in main() and print its embedded message using its built-in explanation method.
+
+#include"iostream"
+
+
