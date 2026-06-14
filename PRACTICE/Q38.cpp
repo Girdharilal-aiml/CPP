6fd375162553c2290cd6a1c28aa9692d99bc1edd
Girdhari lal <girdharilal.aiml@gmail.com>
2026-06-14 13:02:56 +0500

    Update Q38.cpp

--- PRACTICE/Q38.cpp
+++ PRACTICE/Q38.cpp
@@ -1,3 +1,7 @@
 // 24. The Overheating Engine:
 // Rule: Create a custom OverheatError entity containing a currentTemp integer (it does not need to inherit from system errors).
 // Rule: If an Engine entity hits 120 degrees, signal this custom object. Capture the object in main() and extract the currentTemp integer to display it.
+
+#include"iostream"
+
+
