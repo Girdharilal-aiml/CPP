f30db62a197c342b48034c08c4df27453145ea45
Girdhari lal <girdharilal.aiml@gmail.com>
2026-06-14 12:46:15 +0500

    Update Q37.cpp

--- PRACTICE/Q37.cpp
+++ PRACTICE/Q37.cpp
@@ -1,3 +1,7 @@
 // 23. The ATM Withdrawal (Custom Error Entity):
 // Rule: Create a highly specialized error entity named InsufficientFundsError that integrates into the standard C++ error hierarchy. It must override the built-in explanation method to provide a custom warning message when called.
 // Rule: In an Account class, write a withdraw(double amount) function. If the amount exceeds the balance, halt execution and signal this specific InsufficientFundsError object.
+
+#include"iostream"
+
+
