992a547645159bf86f303aa5d1cd58e522b31f25
Girdhari lal <girdharilal.aiml@gmail.com>
2026-06-17 13:29:27 +0500

    Update Q42.cpp

--- PRACTICE/Q42.cpp
+++ PRACTICE/Q42.cpp
@@ -1,2 +1,6 @@
 // 28. Student Records (Raw Memory Storage): AceSchool needs to save a Student entity (containing char name[50], int id, float gpa).
 // Rule: Even though this is standard text/number data, you must save the Student object to records.dat exactly as it appears in RAM-as a single, raw block of bytes. Do not format it line-by-line or use standard text insertion operators (<<). It should look like gibberish if opened in Notepad.
+
+#include "iostream"
+
+
