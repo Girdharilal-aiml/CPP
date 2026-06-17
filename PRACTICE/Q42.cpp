37ae5b4d96864174ebe8d693906e401521adc033
Girdhari lal <girdharilal.aiml@gmail.com>
2026-06-17 13:29:38 +0500

    Update Q42.cpp

--- PRACTICE/Q42.cpp
+++ PRACTICE/Q42.cpp
@@ -2,5 +2,6 @@
 // Rule: Even though this is standard text/number data, you must save the Student object to records.dat exactly as it appears in RAM-as a single, raw block of bytes. Do not format it line-by-line or use standard text insertion operators (<<). It should look like gibberish if opened in Notepad.
 
 #include "iostream"
+#include "fstream"
 
 
