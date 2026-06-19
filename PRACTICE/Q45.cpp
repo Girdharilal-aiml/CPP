c616f5f99783f7f606021f8f64f2690ca38f6d3a
Girdhari lal <girdharilal.aiml@gmail.com>
2026-06-19 18:09:23 +0500

    Update Q45.cpp

--- PRACTICE/Q45.cpp
+++ PRACTICE/Q45.cpp
@@ -5,6 +5,8 @@
 // Rule: The system must strictly prevent a programmer from creating a direct object of a generic satellite. It must only exist as a base idea.
 // Rule: Every satellite must be able to checkSignal().
 
+// 2. Specific Entities: Create RelaySatellite and SpySatellite that implement the general concept.
+// Rule: If a SpySatellite calculates its signal and the value is less than 10.0, it must immediately halt normal execution flow and signal a highly specialized error entity named SignalLostError. This error entity must integrate into the standard C++ error system and provide the custom message: "Critical: Orbital connection severed."
 
 
 
