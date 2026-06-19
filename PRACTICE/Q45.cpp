255475323d12d26ac47df66b00fbb23503ff3d87
Girdhari lal <girdharilal.aiml@gmail.com>
2026-06-19 18:09:07 +0500

    Update Q45.cpp

---
+++ PRACTICE/Q45.cpp
@@ -0,0 +1,13 @@
+// 9. Concepts: Abstraction, Custom Error Entities, Array Traversal (via Pointers), and Text Append Operations.
+// You are designing the communication software for a ground control station.
+
+
+
+
+
+
+
+
+
+
+
