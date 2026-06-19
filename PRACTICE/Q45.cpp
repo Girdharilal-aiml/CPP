4515dcc8f9e1b951888914cb445be69ea167f468
Girdhari lal <girdharilal.aiml@gmail.com>
2026-06-19 18:09:13 +0500

    Update Q45.cpp

--- PRACTICE/Q45.cpp
+++ PRACTICE/Q45.cpp
@@ -1,6 +1,9 @@
 // 9. Concepts: Abstraction, Custom Error Entities, Array Traversal (via Pointers), and Text Append Operations.
 // You are designing the communication software for a ground control station.
 
+// 1. The Core Concept: You have a general concept called Satellite. Every satellite has a designation (string).
+// Rule: The system must strictly prevent a programmer from creating a direct object of a generic satellite. It must only exist as a base idea.
+// Rule: Every satellite must be able to checkSignal().
 
 
 
