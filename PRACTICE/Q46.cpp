eeb2843d99bc072ec2bef3133446470404deddb5
Girdhari lal <girdharilal.aiml@gmail.com>
2026-06-20 19:42:29 +0500

    Update Q46.cpp

---
+++ PRACTICE/Q46.cpp
@@ -0,0 +1,7 @@
+// 10. Concepts: The Diamond Problem, Generic Utility Functions, and Raw Memory (Binary) Storage.
+// You are building the patient intake software for a busy metropolitan hospital.
+
+
+
+
+
