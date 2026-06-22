93944da45577045d84f12006bfb81837d3840afc
Girdhari lal <girdharilal.aiml@gmail.com>
2026-06-22 15:07:35 +0500

    Update Q47.cpp

--- PRACTICE/Q47.cpp
+++ PRACTICE/Q47.cpp
@@ -1,6 +1,10 @@
 // 11. Concepts: Generic Data Structures, Polymorphism, and Standard System Errors.
 // You are programming the autonomous management system for a futuristic smart farm.
 
+// 1. The Storage Facility:
+// Rule: Design a generic storage entity called Silo. It must contain a fixed array capable of holding exactly 100 items of any one specific data type.
+// Rule: Include an integer tracking the current count. Write an addCrop(item) method that adds an item to the array, but silently ignores the action if the Silo is full.
+
 
 
 
