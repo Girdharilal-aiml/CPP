d303518d98a17a3cf5be80784eb3438eb14a10fc
Girdhari lal <girdharilal.aiml@gmail.com>
2026-06-26 14:03:35 +0500

    Update F1.cpp

--- BASICs/F1.cpp
+++ BASICs/F1.cpp
@@ -158,6 +158,7 @@ public:
         cout << "TEACHER : " << teacher->getName() << endl;
         cout << "STUDENTS ENROLLED: " << studentCount << endl;
         for (int i = 0; i < studentCount; i++) {
+            cout << "  -> " << students[i]->getName()
         }
     }
 };
