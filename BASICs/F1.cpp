d96bb409cd2ad42cad1532a2f23dab831949ed88
Girdhari lal <girdharilal.aiml@gmail.com>
2026-06-27 13:38:37 +0500

    Update F1.cpp

--- BASICs/F1.cpp
+++ BASICs/F1.cpp
@@ -200,6 +200,7 @@ public:
         cout << "\n--- STUDENTS ---" << endl;
         for (int i = 0; i < sCount; i++) {
             students[i]->display();
+            students[i]->showRole();  // Polymorphism in action
         }
 
     }
