1dcb11fbd9fb258ae30b56673dc8c1f3546bafb3
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-09-23 11:28:37 +0500

    Update F2.cpp

--- BASICs/F2.cpp
+++ BASICs/F2.cpp
@@ -333,5 +333,14 @@ private:
     string researchArea;
 
 public:
+    Researcher(string n, int a, string cnic, string i, string area)
+        : Person(n, a, cnic, i) {
+        researchArea = area;
+    }
+
+    
+   
+   
+
     
 };
