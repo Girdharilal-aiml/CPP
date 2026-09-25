9fc00f2e0f644f3f1177286237fc15f059e10a3b
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-09-25 12:11:41 +0500

    Update F2.cpp

--- BASICs/F2.cpp
+++ BASICs/F2.cpp
@@ -355,3 +355,8 @@ public:
 
 // Teacher must ALSO be virtual for diamond fix
 // But we already defined Teacher — let's make a VirtualTeacher for demo
+class VirtualTeacher : virtual public Person {
+
+
+
+};
